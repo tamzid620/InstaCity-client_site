f61531926f4f110bf359d0ab489efb6df7236288
tamzid620 <tamzid620@gmail.com>
2025-03-19 13:05:38 +0600

    footer images created

--- src/Components/UserShared/Footer/Footer.jsx
+++ src/Components/UserShared/Footer/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import icon1 from '@/assests/icons/home.png' ;
+import icon2 from '@/assests/icons/tabs.png' ;
+import icon3 from '@/assests/icons/user.png' ;
+import Image from 'next/image';
 
 const Footer = () => {
     return (
-        <footer className='px-4 py-4 bg-blue-950 '>
-            <h1>Footer</h1>
+        <footer className='px-10 py-4 bg-blue-950 flex justify-between items-center'>
+        <div>
+            <Image src={icon1} alt='tab icon' title='home icon' className='w-8'/>
+        </div>
+        <div>
+            <Image src={icon2} alt='tab icon' title='create post' className='w-8'/>
+        </div>
+        <div>
+            <Image src={icon3} alt='tab icon' title='profile' className='w-8'/>
+        </div>
         </footer>
     );
 };
